42d091a94c8eebe3de383afed5eca65548311b99
divyesh2109 <divyesh9123savaliya@gmail.com>
2024-04-08 15:14:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.less'
+import Home from './Home/Home'
+import Header from './component/Header'
+import './assets/style/Style.less'
+import Footer from './component/Footer'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
